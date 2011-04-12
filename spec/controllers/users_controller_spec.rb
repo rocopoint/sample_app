e78912f4d6019ec0f8eee2f1116f90506b1f58bc
Costin Olteanu <costin.olteanu@rocopoint.com>
2011-04-12 22:30:39 +0300

    Added a sidebar and some styling

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -21,17 +21,23 @@ describe UsersController do
 
     it "should have the right title" do
       get :show, :id => @user
-      response.should have_selector("title", :content => @user.name)
+      response.should have_selector('title', :content => @user.name)
     end
 
     it "should have the user's name" do
       get :show, :id => @user
-      response.should have_selector("h1", :content => @user.name)
+      response.should have_selector('h1', :content => @user.name)
     end
 
     it "should have a profile image" do
       get :show, :id => @user
-      response.should have_selector("h1>img", :class => "gravatar")
+      response.should have_selector('h1>img', :class => "gravatar")
+    end
+
+    it "should have the right URL" do
+      get :show, :id => @user
+      response.should have_selector('td>a', :content => user_path(@user),
+                                            :href    => user_path(@user))
     end
 
   end
